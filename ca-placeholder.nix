20717925dec78a005a2fee5e805d3f9f684d1822
Puck Meerburg <puck@puckipedia.com>
2024-10-09 03:07:15 +0000

    Replace large if-else chain with attrset lookup

--- ca-placeholder.nix
+++ ca-placeholder.nix
@@ -1,24 +1,9 @@
 with builtins; let
+  nibbles = "0123456789abcdef";
+  nibbleMap = builtins.listToAttrs (builtins.genList (a: { name = builtins.substring a 1 nibbles; value = a; }) 16);
   hexToDecimal = hex: assert ((stringLength hex) / 2) * 2 == (stringLength hex);
     let
-      nibble = w: if w == "0" then  0
-             else if w == "1" then  1
-             else if w == "2" then  2
-             else if w == "3" then  3
-             else if w == "4" then  4
-             else if w == "5" then  5
-             else if w == "6" then  6
-             else if w == "7" then  7
-             else if w == "8" then  8
-             else if w == "9" then  9
-             else if w == "a" then 10
-             else if w == "b" then 11
-             else if w == "c" then 12
-             else if w == "d" then 13
-             else if w == "e" then 14
-             else if w == "f" then 15
-             else -1;
-      byte = w: (nibble (substring 0 1 w)) * 16 + (nibble (substring 1 1 w));
+      byte = w: nibbleMap.${substring 0 1 w} * 16 + nibbleMap.${substring 1 1 w};
     in
       genList (a: byte (substring (a * 2) 2 hex)) ((stringLength hex) / 2);
 
